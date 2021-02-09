9f0f77393e6fb56e868bcc0189e1894df69a0458
eloc0n <johnisb.21@gmail.com>
2021-02-09 10:56:10 +0200

    scroll animation fixed to support ie 10

--- main.js
+++ main.js
@@ -3,13 +3,15 @@ var grab_dropdown = document.getElementById('grab-dropdown');
 
 
 function changeHeaderSize() {
-    if (window.scrollY > 9) {
+    if (window.pageYOffset > 9) {
+        console.log('> 9 pixels')
         grab_header_top.classList.remove('header-top');
         grab_header_top.classList.add('header-top-lg');
         grab_dropdown.classList.remove('dropdown-content-sm');
         grab_dropdown.classList.add('dropdown-content-lg');
     }
     else {
+        console.log('< 9 pixels')
         grab_header_top.classList.add('header-top');
         grab_header_top.classList.remove('header-top-lg');
         grab_dropdown.classList.add('dropdown-content-sm');
