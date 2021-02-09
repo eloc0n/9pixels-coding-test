4f8c125c2ff3d84e5481ef5071382228d1ee90b2
eloc0n <johnisb.21@gmail.com>
2021-02-09 09:24:11 +0200

    fixed dropdown width on scroll

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
+var grab_header_top = document.getElementById('grab-header-top');
+var grab_dropdown = document.getElementById('grab-dropdown');
+
+
 function changeHeaderSize() {
     if (window.scrollY > 9) {
-        console.log('hii')
-        var element = document.getElementById('grab-header-top');
-        console.log(element)
-        element.classList.remove('header-top');
-        element.classList.add('header-top-lg');
+        grab_header_top.classList.remove('header-top');
+        grab_header_top.classList.add('header-top-lg');
+        grab_dropdown.classList.remove('dropdown-content-sm');
+        grab_dropdown.classList.add('dropdown-content-lg');
     }
     else {
-        var element = document.getElementById('grab-header-top');
-        element.classList.remove('header-top-lg');
-        element.classList.add('header-top');
+        grab_header_top.classList.add('header-top');
+        grab_header_top.classList.remove('header-top-lg');
+        grab_dropdown.classList.add('dropdown-content-sm');
+        grab_dropdown.classList.remove('dropdown-content-lg');
     }
 }
 
